50385df70309e3420a5063c4c1914f708105065d
hyeon22 <ski0123@naver.com>
2025-02-23 11:41:53 +0900

    first commit

---
+++ src/main/java/com/example/sessionpractice2/SessionPractice2Application.java
@@ -0,0 +1,13 @@
+package com.example.sessionpractice2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SessionPractice2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SessionPractice2Application.class, args);
+    }
+
+}
